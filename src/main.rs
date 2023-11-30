13cd565d6baf4feebfdb780e2074a32475e1d401
Mark Smith <smith.h.mark@gmail.com>
2023-11-30 16:34:01 -0700

    Adding in random enemy spawns

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-use std::task::Wake;
-
 use bevy::{prelude::*, window::PrimaryWindow};
+use rand::prelude::*;
 
 const PLAYER_SPEED: f32 = 500.0;
 const PLAYER_SIZE: f32 = 64.0;
+
+const NUMBER_OF_ENEMIES: usize = 4;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, spawn_camera)
         .add_systems(Startup, spawn_player)
+        .add_systems(Startup, spawn_enemies)
         .add_systems(Update, player_movement)
         .add_systems(Update, confine_player_movement)
         .run();
@@ -17,6 +19,9 @@ fn main() {
 
 #[derive(Component)]
 struct Player {}
+
+#[derive(Component)]
+struct Enemy {}
 
 fn spawn_player(
     mut commands: Commands,
@@ -52,6 +57,30 @@ fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<Primar
     });
     /*
      */
+}
+
+fn spawn_enemies(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    for enemy_number in 0..NUMBER_OF_ENEMIES {
+        let random_x = random::<f32>() * window.width();
+        let random_y = random::<f32>() * window.height();
+
+        println!("enemy: {enemy_number} @ {random_x},{random_y}");
+
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform::from_xyz(random_x, random_y, 0.0),
+                texture: asset_server.load("sprites/ball_red_large.png"),
+                ..default()
+            },
+            Enemy {},
+        ));
+    }
 }
 
 fn player_movement(
