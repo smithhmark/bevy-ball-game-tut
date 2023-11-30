4c7a550ac422ec23b64c8aee1fb58564c5efb616
Mark Smith <smith.h.mark@gmail.com>
2023-11-30 15:29:46 -0700

    Adding readme and first tutorial vid done

---
+++ README.md
@@ -0,0 +1,8 @@
+= Ball Game Tutorial =
+
+This is a working through of the Ball Game Tutorial by YouTuber [Jaques]( https://www.youtube.com/@jacques-dev). See the play list: [here](https://www.youtube.com/playlist?list=PLVnntJRoP85JHGX7rGDu6LaF3fmDDbqyd)
+
+**NOTE:** Jacques' original tutorial is for Bevy v0.10. I am updating the example to v0.12 as I go.
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,58 @@
+use bevy::{prelude::*, window::PrimaryWindow};
+
 fn main() {
-    println!("Hello, world!");
-    println!("world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        //.add_systems(Startup, setup)
+        .add_systems(Startup, spawn_camera)
+        .add_systems(Startup, spawn_player)
+        .run();
+}
+
+#[derive(Component)]
+struct Player {}
+/*
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("sprites/ball_blue_large.png"),
+        ..default()
+    });
+}
+*/
+
+fn spawn_player(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    println!("spawning player");
+    println!(
+        "{}, {}, {}",
+        window.width() / 2.0,
+        window.height() / 2.0,
+        0.0
+    );
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+            texture: asset_server.load("sprites/ball_blue_large.png"),
+            ..default()
+        },
+        Player {},
+    ));
+}
+
+fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+    let window = window_query.get_single().unwrap();
+    println!("spawning camera");
+    //commands.spawn(Camera2dBundle::default());
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        ..default()
+    });
+    /*
+     */
 }
