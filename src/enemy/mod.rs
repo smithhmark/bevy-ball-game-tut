5a04201165613e35117dd3c939952f47402e9fd5
Mark Smith <smith.h.mark@gmail.com>
2023-11-30 21:41:58 -0700

    inital system ordering: move then check positions

--- src/enemy/mod.rs
+++ src/enemy/mod.rs
@@ -17,10 +17,10 @@ pub struct EnemyPlugin;
 impl Plugin for EnemyPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<EnemySpawnTimer>()
-            .add_systems(Update, tick_enemy_spawn_timer)
             .add_systems(Startup, spawn_enemies)
-            .add_systems(Update, enemy_movement)
+            .add_systems(Update, tick_enemy_spawn_timer)
             .add_systems(Update, spawn_enemies_over_time)
+            .add_systems(Update, enemy_movement.before(update_enemy_direction))
             .add_systems(Update, update_enemy_direction);
     }
 }
