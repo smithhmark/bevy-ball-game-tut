e5d8024f62edc2594f000022875456f1bd949d88
Mark Smith <smith.h.mark@gmail.com>
2023-11-30 18:59:52 -0700

    spawning stars over time

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,13 @@ const ENEMY_SIZE: f32 = 64.0;
 
 const NUMBER_OF_STARS: usize = 10;
 const STAR_SIZE: f32 = 30.0;
+const STAR_SPAWN_TIME: f32 = 1.0;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .init_resource::<Score>()
+        .init_resource::<StarSpawnTimer>()
         .add_systems(Startup, spawn_camera)
         .add_systems(Startup, spawn_player)
         .add_systems(Startup, spawn_enemies)
@@ -26,6 +28,8 @@ fn main() {
         .add_systems(Update, enemy_hit_player)
         .add_systems(Update, player_hit_star)
         .add_systems(Update, update_score)
+        .add_systems(Update, tick_star_spawn_timer)
+        .add_systems(Update, spawn_stars_over_time)
         .run();
 }
 
@@ -48,6 +52,19 @@ struct Score {
 impl Default for Score {
     fn default() -> Self {
         Score { value: 0 }
+    }
+}
+
+#[derive(Resource)]
+struct StarSpawnTimer {
+    timer: Timer,
+}
+
+impl Default for StarSpawnTimer {
+    fn default() -> Self {
+        Self {
+            timer: Timer::from_seconds(STAR_SPAWN_TIME, TimerMode::Repeating),
+        }
     }
 }
 
@@ -302,5 +319,30 @@ fn enemy_hit_player(
 fn update_score(score: Res<Score>) {
     if score.is_changed() {
         println!("Score: {}", score.value.to_string());
+    }
+}
+
+fn tick_star_spawn_timer(mut star_spawn_timer: ResMut<StarSpawnTimer>, time: Res<Time>) {
+    star_spawn_timer.timer.tick(time.delta());
+}
+
+fn spawn_stars_over_time(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+    star_spawn_timer: Res<StarSpawnTimer>,
+) {
+    if star_spawn_timer.timer.finished() {
+        let window = window_query.get_single().unwrap();
+        let random_x = random::<f32>() * window.width();
+        let random_y = random::<f32>() * window.height();
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform::from_xyz(random_x, random_y, 0.0),
+                texture: asset_server.load("sprites/star.png"),
+                ..default()
+            },
+            Star {},
+        ));
     }
 }
