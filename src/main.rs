41442ddd04fa3ae0c2a53dc5152e5be4e6269b45
Mark Smith <smith.h.mark@gmail.com>
2023-11-30 19:51:15 -0700

    using events to update high scores list

--- src/main.rs
+++ src/main.rs
@@ -19,14 +19,16 @@ fn main() {
         .init_resource::<Score>()
         .init_resource::<StarSpawnTimer>()
         .init_resource::<EnemySpawnTimer>()
+        .init_resource::<HighScores>()
+        .add_event::<GameOver>()
         .add_systems(Startup, spawn_camera)
         .add_systems(Startup, spawn_player)
         .add_systems(Startup, spawn_enemies)
         .add_systems(Startup, spawn_stars)
         .add_systems(Update, player_movement)
         .add_systems(Update, confine_player_movement)
-        .add_systems(Update, (enemy_movement, update_enemy_direction))
-        //.add_systems(Update, update_enemy_direction)
+        .add_systems(Update, enemy_movement)
+        .add_systems(Update, update_enemy_direction)
         .add_systems(Update, enemy_hit_player)
         .add_systems(Update, player_hit_star)
         .add_systems(Update, update_score)
@@ -34,7 +36,10 @@ fn main() {
         .add_systems(Update, tick_enemy_spawn_timer)
         .add_systems(Update, spawn_stars_over_time)
         .add_systems(Update, exit_game)
-        //.add_systems(Update, spawn_enemies_over_time)
+        .add_systems(Update, spawn_enemies_over_time)
+        .add_systems(Update, handle_game_over)
+        .add_systems(Update, update_high_scores)
+        .add_systems(Update, high_scores_updated)
         .run();
 }
 
@@ -57,6 +62,17 @@ struct Score {
 impl Default for Score {
     fn default() -> Self {
         Score { value: 0 }
+    }
+}
+
+#[derive(Resource)]
+struct HighScores {
+    scores: Vec<(String, u32)>,
+}
+
+impl Default for HighScores {
+    fn default() -> Self {
+        Self { scores: Vec::new() }
     }
 }
 
@@ -84,6 +100,11 @@ impl Default for StarSpawnTimer {
             timer: Timer::from_seconds(STAR_SPAWN_TIME, TimerMode::Repeating),
         }
     }
+}
+
+#[derive(Event)]
+struct GameOver {
+    score: u32,
 }
 
 fn spawn_player(
@@ -309,9 +330,11 @@ fn player_hit_star(
 
 fn enemy_hit_player(
     mut commands: Commands,
+    mut game_over_event_writer: EventWriter<GameOver>,
     mut player_query: Query<(Entity, &Transform), With<Player>>,
     enemy_query: Query<&Transform, With<Enemy>>,
     asset_server: Res<AssetServer>,
+    score: Res<Score>,
 ) {
     if let Ok((player_entity, player_transform)) = player_query.get_single_mut() {
         let player_radius = PLAYER_SIZE / 2.0;
@@ -328,6 +351,7 @@ fn enemy_hit_player(
                     settings: PlaybackSettings::DESPAWN,
                 });
                 commands.entity(player_entity).despawn();
+                game_over_event_writer.send(GameOver { score: score.value });
             }
         }
     }
@@ -393,5 +417,29 @@ fn spawn_enemies_over_time(
 fn exit_game(keyboard_input: Res<Input<KeyCode>>, mut app_exit_event_writer: EventWriter<AppExit>) {
     if keyboard_input.just_pressed(KeyCode::Escape) {
         app_exit_event_writer.send(AppExit);
+    }
+}
+
+fn handle_game_over(mut game_over_event_reader: EventReader<GameOver>) {
+    for event in game_over_event_reader.read() {
+        println!("Your final score is: {}", event.score);
+    }
+}
+
+fn update_high_scores(
+    mut game_over_event_reader: EventReader<GameOver>,
+    mut high_scores: ResMut<HighScores>,
+) {
+    for event in game_over_event_reader.read() {
+        println!("Your final score is: {}", event.score);
+        high_scores
+            .scores
+            .push(("Player One".to_string(), event.score));
+    }
+}
+
+fn high_scores_updated(high_scores: Res<HighScores>) {
+    if high_scores.is_changed() {
+        println!("High scores: {:?}", high_scores.scores);
     }
 }
